0fdcb832b39356a501af07f4efb2ca949c411590
Edward Shen <edwardshen24@gmail.com>
2024-06-08 22:59:55 -0700

    added modal

--- index.js
+++ index.js
@@ -48,31 +48,55 @@ $(document).ready(function(){
       });
   });
 
-
-/*
-let currentSlideIndices = [0, 0, 0, 0]; // Track the current slide for each project
-function showSlide(projectIndex, slideIndex) {
-    const carouselInner = document.querySelectorAll('.carousel-inner')[projectIndex];
-    const slides = carouselInner.querySelectorAll('img');
-    if (slideIndex >= slides.length) {
-        currentSlideIndices[projectIndex] = 0;
-    } else if (slideIndex < 0) {
-        currentSlideIndices[projectIndex] = slides.length - 1;
-    } else {
-        currentSlideIndices[projectIndex] = slideIndex;
+var width = $(window).width(); 
+window.onscroll = function(){
+if ((width >= 1000)){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $("#header").css("background","#fff");
+        $("#header").css("color","#000");
+        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+        $("#header").css("padding","4vh 4vw");
+        $("#navigation a").hover(function(){
+            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+        },function(){
+            $(this).css("border-bottom","2px solid transparent");
+        });
+    }else{
+        $("#header").css("background","transparent");
+        $("#header").css("color","#fff");
+        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+        $("#header").css("padding","6vh 4vw");
+        $("#navigation a").hover(function(){
+            $(this).css("border-bottom","2px solid #fff");
+        },function(){
+            $(this).css("border-bottom","2px solid transparent");
+        });
     }
-    const newTransform = -currentSlideIndices[projectIndex] * 100 + '%';
-    carouselInner.style.transform = `translateX(${newTransform})`;
+}
 }
 
-function nextSlide(projectIndex) {
-    showSlide(projectIndex, currentSlideIndices[projectIndex] + 1);
-}
 
-function prevSlide(projectIndex) {
-    showSlide(projectIndex, currentSlideIndices[projectIndex] - 1);
-}
-*/
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1650);
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('body,html').animate({
+        scrollTop: $(hash).offset().top
+        }, 1800, function(){
+        window.location.hash = hash;
+       });
+       } 
+      });
+  });
 
 document.addEventListener("DOMContentLoaded", function() {
     const projectCount = document.querySelectorAll('.project').length;
@@ -103,4 +127,29 @@ document.addEventListener("DOMContentLoaded", function() {
     // Attach the functions to the window object so they can be called from the HTML
     window.nextSlide = nextSlide;
     window.prevSlide = prevSlide;
+
+    // Modal functionality
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    const captionText = document.getElementById("caption");
+    const closeModal = document.getElementsByClassName("close")[0];
+
+    document.querySelectorAll('.project img').forEach((img, index) => {
+        img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        }
+    });
+
+    closeModal.onclick = function() { 
+        modal.style.display = "none";
+    }
+
+    // Close the modal if user clicks outside the image
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
